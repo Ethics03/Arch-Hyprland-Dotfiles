f418316ceb53b89930a1f243dd6bef6e301e8cc7
Ethics03 <honey032004@gmail.com>
2025-01-28 16:15:37 +0530

    Hypr dot with scripts

---
+++ hypr/scripts/wall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Path to your wallpapers directory
+WALLPAPER_DIR="$HOME/Pictures/wallpapers"
+
+# Find all image files in the directory (can adjust file extensions if needed)
+WALLPAPERS=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.gif" \))
+
+# Choose a random wallpaper
+RANDOM_WALLPAPER=$(echo "$WALLPAPERS" | shuf -n 1)
+
+# Set the wallpaper using Hyprpaper
+hyprctl hyprpaper preload "${RANDOM_WALLPAPER}"
+hyprctl hyprpaper wallpaper "eDP-1,${RANDOM_WALLPAPER}"
